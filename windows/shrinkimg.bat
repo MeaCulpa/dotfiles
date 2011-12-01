06c806daf75653669cb2de052aef04ab4a9a4a61
Mea Culpa <infernoxu@gmail.com>
2011-12-01 13:53:07 +0800

    sed=>awk in windows

--- windows/shrinkimg.bat
+++ windows/shrinkimg.bat
@@ -1,1 +1,1 @@
-ls -1 *.jpg | sed "s/\(.*\)\.JPG/E:\\imagemagick\\convert.exe -resize 50%%%% & \1_.jpg/g" > shrink.bat
+ls -1 *.jpg | awk " {system(\"D:\\imagemagick\\convert.exe -resize 50% \"$1\" s_\"$1)} "
