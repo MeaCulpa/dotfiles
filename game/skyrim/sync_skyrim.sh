dfa2726766effe042b4f0c80881c0d8d2125ebb4
Mea Culpa <infernoxu@gmail.com>
2012-09-22 21:35:44 +0800

    skyrim

--- game/skyrim/sync_skyrim.sh
+++ game/skyrim/sync_skyrim.sh
@@ -3,8 +3,13 @@
 #rsync -nrtz --progress --exclude-from=/cygdrive/d/bkskyrim/dump/exclude.txt --delete --stats /cygdrive/d/Skyrim/Data /cygdrive/h/skyrim/dump
 #rsync -nrtz --progress --exclude-from=/cygdrive/d/bkskyrim/dump/exclude.txt --delete --stats /cygdrive/d/Skyrim/Data /cygdrive/h/skyrim/dump
 # From Cygsin To Linux
-rsync -nrtz --progress --exclude-from=/cygdrive/d/bkskyrim/dump/exclude.txt --delete --stats /cygdrive/d/Skyrim/Data inferno@192.168.11.3:/data/w1/Skyrim
+#rsync -nrtz --progress --exclude-from=/cygdrive/d/bkskyrim/dump/exclude.txt --delete --stats /cygdrive/d/Skyrim/Data inferno@192.168.11.3:/data/w1/Skyrim
+
+rsync -avz --exclude-from=exclude.txt --delete --stats /cygdrive/g/st/steamapps/common/skyrim/Data inferno@192.168.11.3:/data/w1/steam/steamapps/common/skyrim/
+rsync -avz --delete --stats /cygdrive/g/st/steamapps/common/skyrim/Mopy inferno@192.168.11.3:/data/w1/steam/steamapps/common/skyrim/
+rsync -avz --delete --stats /cygdrive/g/st/steamapps/common/skyrim/BOSS inferno@192.168.11.3:/data/w1/steam/steamapps/common/skyrim/
+
 
 # From USB To Linux
-rsync -nrtz --exclude-from=/mnt/usb/skyrim/dump/exclude.txt --delete --progress --stats /mnt/usb/skyrim/dump/Data /data/w1/Skyrim
+#rsync -nrtz --exclude-from=/mnt/usb/skyrim/dump/exclude.txt --delete --progress --stats /mnt/usb/skyrim/dump/Data /data/w1/Skyrim
 
